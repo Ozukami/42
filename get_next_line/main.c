f10fa63293909b8da3413ea1722609a2e675fc8a
Arthur POISSON <apoisson@e3r13p1.42.fr>
2016-12-03 14:45:26 +0100

    gnl : plus que 1 leak

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: apoisson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/20 11:12:54 by apoisson          #+#    #+#             */
-/*   Updated: 2016/12/03 12:26:05 by apoisson         ###   ########.fr       */
+/*   Updated: 2016/12/03 14:40:39 by apoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int		main(int ac, char **av)
 	char	*line;
 	int		fd;
 	int		fd2;
+	int		fd3;
 
 	fd = 0;
 	if (ac == 1)
@@ -32,28 +33,38 @@ int		main(int ac, char **av)
 		while (get_next_line(fd, &line) > 0)
 			ft_putendl(line);
 	}
-	else if (ac == 3)
+	else if (ac == 4)
 	{
 		fd = open(av[1], O_RDONLY);
 		fd2 = open(av[2], O_RDONLY);
+		fd3 = open(av[3], O_RDONLY);
+		get_next_line(fd, &line);
+		ft_putendl(line);
+		get_next_line(fd2, &line);
+		ft_putendl(line);
+		get_next_line(fd3, &line);
+		ft_putendl(line);
+		get_next_line(fd3, &line);
+		ft_putendl(line);
+		get_next_line(fd, &line);
+		ft_putendl(line);
 		get_next_line(fd, &line);
 		ft_putendl(line);
 		get_next_line(fd2, &line);
 		ft_putendl(line);
 		get_next_line(fd, &line);
 		ft_putendl(line);
-		get_next_line(fd, &line);
+		get_next_line(fd2, &line);
 		ft_putendl(line);
 		get_next_line(fd2, &line);
 		ft_putendl(line);
-		get_next_line(fd, &line);
+		get_next_line(fd3, &line);
 		ft_putendl(line);
-		get_next_line(fd2, &line);
+		get_next_line(fd3, &line);
 		ft_putendl(line);
-		get_next_line(fd, &line);
-		ft_putendl(line);
-		get_next_line(fd2, &line);
-		ft_putendl(line);
+		close(fd);
+		close(fd2);
+		close(fd3);
 	}
 	return (0);
 }
