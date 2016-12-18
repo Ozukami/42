9b2fec59c2eae63873a8eebd5f819218af727ddf
Arthur POISSON <apoisson@e1r5p21.42.fr>
2016-12-18 04:29:54 +0100

    strncpy

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: apoisson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 13:08:23 by apoisson          #+#    #+#             */
-/*   Updated: 2016/11/04 13:34:20 by apoisson         ###   ########.fr       */
+/*   Updated: 2016/12/18 03:21:07 by apoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,10 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t i;
 
-	i = 0;
-	while (src[i] && i < len)
-	{
+	i = -1;
+	while (src[++i] && i < len)
 		dst[i] = src[i];
-		i++;
-	}
 	while (i < len)
-	{
-		dst[i] = '\0';
-		i++;
-	}
+		dst[i++] = '\0';
 	return (dst);
 }
