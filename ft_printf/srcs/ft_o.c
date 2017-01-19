9e8f8d281651ab7fc3fef45b37294e2cfbea9bae
Quentin MAUJEAN <qumaujea@e1r6p3.42.fr>
2017-01-19 12:31:44 +0100

    ft_o 80% justify marche pas

--- ft_printf/srcs/ft_o.c
+++ ft_printf/srcs/ft_o.c
@@ -6,7 +6,7 @@
 /*   By: qumaujea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/19 11:37:22 by qumaujea          #+#    #+#             */
-/*   Updated: 2017/01/19 11:42:30 by qumaujea         ###   ########.fr       */
+/*   Updated: 2017/01/19 12:30:34 by qumaujea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,11 @@ size_t		ft_fp_o(size_t len, t_conv *list)
 		size = len;
 	else
 		size = (size_t)list->field;
-	if (list->left && list->prefix && ((int)len < list->p))
+	if (list->left && list->prefix && ((int)len < list->p) && list->left == 0)
 		size = size + 2;
+	if (list->prefix && (int)len > list->field && (int)len > list->p && !list->left)
+		size++;
+	printf("[SIZE = %zu]\n", size);
 	return (size);
 }
 
@@ -41,6 +44,8 @@ void		ft_p_o(char **to_print, t_conv *list, size_t len, size_t size)
 	size_t	i;
 
 	size = (list->p < (int)len) ? size : len;
+	if ((int)len > list->field && (int)len > list->p && list->prefix)
+		(to_print)[0][0] = '0';
 	if (list->p > -1 || list->zero)
 	{
 		i = 0;
@@ -76,10 +81,7 @@ int			ft_left_o(char *arg, size_t len, char **to_print, t_conv *list)
 			ft_strncpy(*to_print + (list->prefix) * 2, arg, (size_t)
 					(ft_min((int)ft_strlen(arg), (int)len)));
 		if (list->prefix)
-		{
 			to_print[0][0] = '0';
-			to_print[0][1] = 'x';
-		}
 		return (1);
 	}
 	return (0);
@@ -87,18 +89,12 @@ int			ft_left_o(char *arg, size_t len, char **to_print, t_conv *list)
 
 void		ft_sub_o2(t_conv *list, char *to_print, char *arg, size_t *i)
 {
-	if (list->field > (int)ft_strlen(arg) && (int)ft_strlen(arg) > list->p)
-	{
-		to_print[*i - (2 + ((list->p == -1 && list->zero) ? 1 : 0))] = '0';
-		to_print[*i - (1 + ((list->p == -1
-						&& list->zero) ? 1 : 0))] = 'x';
-	}
+	if (list->field > (int)ft_strlen(arg) && list->p == -1)
+		to_print[*i - (2 + ((list->p == -1) ? -1 : -2))] = '0';
+	else if (list->field > (int)ft_strlen(arg) && (int)ft_strlen(arg) > list->p)
+		to_print[*i - (2 + ((list->p == -1) ? 0 : -1))] = '0';
 	else
-	{
 		to_print[((int)ft_strlen(arg) < list->p) ? 0 : *i] = '0';
-		to_print[((int)ft_strlen(arg) < list->p) ? 1 : *i + 1] = 'x';
-		*i += 2;
-	}
 }
 
 void		ft_sub_o1(t_conv *list, char *to_print, char *arg, size_t len)
@@ -113,9 +109,7 @@ void		ft_sub_o1(t_conv *list, char *to_print, char *arg, size_t len)
 				|| (list->p == -1 && list->field == -1)
 				|| (list->p <= (int)ft_strlen(arg)
 					&& (list->field < (int)ft_strlen(arg)))))
-	{
 		ft_sub_o2(list, to_print, arg, &i);
-	}
 	if (list->p > -1)
 	{
 		while ((int)i + ft_max(list->p, (int)len) < (int)len
