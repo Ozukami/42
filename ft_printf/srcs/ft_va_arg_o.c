1a4ec42006a082e368660a4d1bd4e1ccadd3c3c7
Quentin MAUJEAN <qumaujea@e1r6p3.42.fr>
2017-01-19 11:49:26 +0100

    Makefile va arg o

---
+++ ft_printf/srcs/ft_va_arg_o.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_va_arg_o.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qumaujea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/19 11:47:18 by qumaujea          #+#    #+#             */
+/*   Updated: 2017/01/19 11:48:07 by qumaujea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t		ft_va_arg_o(va_list ap, t_conv *list, char **str)
+{
+	char	*arg;
+	char	*to_print;
+	size_t	len;
+
+	arg = ft_itoa_base(va_arg(ap, int), 8, 0);
+	len = ft_fp_o(ft_strlen(arg), list);
+	to_print = ft_strspace(len);
+	if (!ft_left_o(arg, len, &to_print, list))
+	{
+		ft_sub_o1(list, to_print, arg, len);
+		if (list->prefix && (list->field == -1 || (list->p == -1
+						&& list->field < (int)ft_strlen(arg))))
+		{
+			*str = ft_strjoin(*str, to_print);
+			//ft_putstr(ft_strjoin("0x", to_print));
+			return (ft_strlen(to_print) + 2);
+		}
+	}
+	*str = ft_strjoin(*str, to_print);
+	//ft_putstr(to_print);
+	return (ft_strlen(to_print));
+}
