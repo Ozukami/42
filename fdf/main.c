a66f46b94b33e10dedf5223209fcbbc0a1b1bbb0
Arthur POISSON <apoisson@e1r5p4.42.fr>
2017-02-23 04:34:48 +0100

    bresenham OK (a normer) + 3D iso OK + bon debut de gestion de l'axe Z (inversion a gerer)

--- fdf/main.c
+++ fdf/main.c
@@ -6,55 +6,28 @@
 /*   By: qumaujea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/10 23:04:32 by qumaujea          #+#    #+#             */
-/*   Updated: 2017/02/12 16:50:18 by qumaujea         ###   ########.fr       */
+/*   Updated: 2017/02/23 04:32:44 by apoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/* NON FONCTIONEL C'EST LE TRUC QUE JE TRAVAILLE POUR L'INSTANT
-double		ft_error_increase(double dy, double dx)
-{
-	double		error_inc;
-
-	if (dx < dy)
-		error_inc = dx / dy;
-	else
-		error_inc = dy / dx;
-	return (error_inc);
-
-}
-
-void	ft_rework_location(int *x, int *y, int *x_end, int *y_end)
+void	ft_rework_location(int *x, int *y, int *x2, int *y2)
 {
 	int		tmp;
 
 	tmp = 0;
-	if (*x > *x2 && *y > *y2)
+	if (*x > *x2)
 	{
 		tmp = *x;
 		*x = *x2;
 		*x2 = tmp;
-		tmp = 0;
+
 		tmp = *y;
 		*y = *y2;
 		*y2 = *y;
 	}
-
 }
-
-void		ft_drw_line(t_env *env, int x, int y, int x_end, int y_end)
-{
-	double	err_value;
-	double	err_inc;
-	double	dx;
-	double	dy;
-
-	err_value = 0;
-	ft_rework_location(&x, &y, &x_end, &y_end)
-	err_inc = ft_error_increase(dy, dx);
-}
-*/
 
 int			ft_tab_size(char **tab)
 {
@@ -88,6 +61,21 @@ void		ft_moulisplit(t_env *env)
 	}
 }
 
+void		bres(t_env *env, int x, int y, int x2, int y2, int color);
+
+void		ft_test(t_env *env, int x, int y, int x2, int y2, int color, int z, int z2)
+{
+	int x_tmp = (x-y);
+	int y_tmp = (x+y) / 1.5;
+	int x2_tmp = (x2-y2);
+	int y2_tmp = (x2+y2) / 1.5;
+
+	bres(env, x_tmp + ((env->win_x * env->win_size) / 2),
+			y_tmp + (env->win_y * env->win_size / 3) - z,
+			x2_tmp + ((env->win_x * env->win_size) / 2),
+			y2_tmp + (env->win_y * env->win_size / 3) - z2, color);
+}
+
 int			main(int ac, char **av)
 {
 	int		i;
@@ -96,7 +84,7 @@ int			main(int ac, char **av)
 
 	if (ac < 2)
 		return (0);
-	env = ft_new_env(av[1], 20);
+	env = ft_new_env(av[1], 50);
 	if (!(fd = open(av[1], O_RDONLY)))
 		exit(0);
 	(env->data)->content_file = malloc(sizeof(char *) * (env->win_y + 1));
@@ -120,40 +108,88 @@ int			main(int ac, char **av)
 		printf("\n");
 		y++;
 	}
-	env->win = mlx_new_window(env->mlx, 500, 500/*env->win_x * env->win_size,
-			env->win_y * env->win_size*/, "FdF");
-	ft_draw_shit2_x(env, 250, 250, 350, 300);
-	ft_draw_shit2_x(env, 250, 250, 350, 350);
-	ft_draw_shit2_x(env, 250, 250, 350, 250);
-	ft_draw_shit2_x(env, 250, 250, 350, 200);
-	ft_draw_shit2_x(env, 250, 250, 350, 150);
-
-	ft_draw_shit_x(env, 250, 250, 150, 300);
-	ft_draw_shit_x(env, 250, 250, 150, 350);
-	ft_draw_shit_x(env, 250, 250, 150, 250);
-	ft_draw_shit_x(env, 250, 250, 150, 200);
-	ft_draw_shit_x(env, 250, 250, 150, 150);
-	
 	/*
-//	ft_draw_shit2_y(env, 250, 250, 250, 150);
-//	ft_draw_shit2_y(env, 250, 250, 200, 150);
-//	ft_draw_shit2_y(env, 250, 250, 150, 150);
-//	ft_draw_shit2_y(env, 250, 250, 300, 150);
-//	ft_draw_shit2_y(env, 250, 250, 350, 150);
-
-//	ft_draw_shit_y(env, 250, 250, 150, 350);
-	ft_draw_shit_y(env, 250, 250, 200, 350); -> fonctionne presque bizarrement
-//	ft_draw_shit_y(env, 250, 250, 250, 350);
-//	ft_draw_shit_y(env, 250, 250, 300, 350);
-//	ft_draw_shit_y(env, 250, 250, 350, 350);
-	ft_brensenham(env, 100, 100, 100, 200, 0x00FF0000);
-	ft_brensenham(env, 100, 100, 200, 100, 0x00FFFF00);
-	ft_brensenham(env, 100, 100, 50, 0, 0x000000FF);
-	ft_brensenham(env, 100, 100, 100, 0, 0x0000FF00);
-	ft_brensenham(env, 100, 100, 0, 100, 0x00FF00FF);
-	ft_brensenham(env, 100, 100, 0, 150, 0x0000FFFF);
-	ft_brensenham(env, 100, 100, 150, 0, 0x00Fb4800);
+	env->win_x = 1020;
+	env->win_y = 1020;
 	*/
+	env->win = mlx_new_window(env->mlx,
+			env->win_x * env->win_size,
+			env->win_y * env->win_size,
+			"FdF");
+	
+	//int	c0 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 10, c5 = 0, c6 = 0, c7 = 0, c8 = 0;
+	//int		tab[4][4] = {{0,0,0,0}, {0,100,0,0}, {0,100,0,0}, {0,0,0,0}};
+
+	i = 0;
+	int	j = 0;
+
+	while (i < env->win_y - 1)
+	{
+		j = 0;
+		while (j < env->win_x - 1)
+		{
+			ft_test(env, i * 15, j * 15, (i + 1) * 15, j * 15, 0x00ff0000,
+					(int)((env->data)->map)[i][j], (int)((env->data)->map)[i + 1][j]);
+			ft_test(env, i * 15, j * 15, i * 15, (j + 1) * 15, 0x00ff0000,
+					(int)((env->data)->map)[i][j], (int)((env->data)->map)[i][j + 1]);
+			j++;
+		}
+		printf("OK\n");
+		i++;
+	}
+
+	/*
+	ft_test(env, 0, 0, 500, 0, 0x00ff0000);
+	ft_test(env, 0, 50, 500, 50, 0x00ff0000);
+	ft_test(env, 0, 100, 500, 100, 0x00ff0000);
+	ft_test(env, 0, 150, 500, 150, 0x00ff0000);
+	ft_test(env, 0, 200, 500, 200, 0x00ff0000);
+	ft_test(env, 0, 250, 500, 250, 0x00ff0000);
+	ft_test(env, 0, 300, 500, 300, 0x00ff0000);
+	ft_test(env, 0, 350, 500, 350, 0x00ff0000);
+	ft_test(env, 0, 400, 500, 400, 0x00ff0000);
+	ft_test(env, 0, 450, 500, 450, 0x00ff0000);
+	ft_test(env, 0, 500, 500, 500, 0x00ff0000);
+
+	ft_test(env, 0, 0, 0, 500, 0x00ff0000);
+	ft_test(env, 50, 0, 50, 500, 0x00ff0000);
+	ft_test(env, 100, 0, 100, 500, 0x00ff0000);
+	ft_test(env, 150, 0, 150, 500, 0x00ff0000);
+	ft_test(env, 200, 0, 200, 500, 0x00ff0000);
+	ft_test(env, 250, 0, 250, 500, 0x00ff0000);
+	ft_test(env, 300, 0, 300, 500, 0x00ff0000);
+	ft_test(env, 350, 0, 350, 500, 0x00ff0000);
+	ft_test(env, 400, 0, 400, 500, 0x00ff0000);
+	ft_test(env, 450, 0, 450, 500, 0x00ff0000);
+	ft_test(env, 500, 0, 500, 500, 0x00ff0000);
+	*/
+
+	/*
+	   mlx_do_sync(env->mlx);
+
+	   bres(env, 250, 225, 250, 150, 0x00ff0000);
+	   bres(env, 263, 225, 300, 150, 0x0000FF00);
+	   bres(env, 275, 225, 350, 150, 0x000000ff);
+	   bres(env, 275, 237, 350, 200, 0x00ff00ff);
+
+	   bres(env, 275, 250, 350, 250, 0x00ff0000);
+	   bres(env, 275, 263, 350, 300, 0x0000ff00);
+	   bres(env, 275, 275, 350, 350, 0x000000ff);
+	   bres(env, 263, 275, 300, 350, 0x00FF00ff);
+
+	   bres(env, 250, 275, 250, 350, 0x00ff0000);
+	   bres(env, 237, 275, 200, 350, 0x0000FF00);
+	   bres(env, 225, 275, 150, 350, 0x000000ff);
+	   bres(env, 225, 263, 150, 300, 0x00ff00ff);
+
+	   bres(env, 225, 250, 150, 250, 0x00ff0000);
+	   bres(env, 225, 237, 150, 200, 0x0000ff00);
+	   bres(env, 225, 225, 150, 150, 0x000000ff);
+	   bres(env, 237, 225, 200, 150, 0x00FF00FF);
+
+	   mlx_do_sync(env->mlx);
+	   */
+
 	mlx_loop(env->mlx);
 	return (0);
 }
