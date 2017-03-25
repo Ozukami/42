c407a55b79af2754327c73e3f8c836d94bc85c8f
Arthur POISSON <apoisson@e1r3p4.42.fr>
2017-03-26 00:33:42 +0100

    remplacement des malloc par memalloc

--- printf/libft/ft_strzero.c
+++ printf/libft/ft_strzero.c
@@ -6,7 +6,7 @@
 /*   By: qumaujea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/02 01:26:49 by qumaujea          #+#    #+#             */
-/*   Updated: 2017/03/11 06:40:34 by apoisson         ###   ########.fr       */
+/*   Updated: 2017/03/26 00:33:14 by apoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strzero(size_t size)
 {
 	char	*str;
 
-	if (!(str = malloc(size + 1)))
+	if (!(str = ft_memalloc(size + 1)))
 		return (NULL);
 	ft_fill_zero(str, size + 1);
 	return (str);
--- printf/srcs/ft_data.c
+++ printf/srcs/ft_data.c
@@ -6,7 +6,7 @@
 /*   By: apoisson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/01 00:57:08 by apoisson          #+#    #+#             */
-/*   Updated: 2017/03/17 05:39:57 by apoisson         ###   ########.fr       */
+/*   Updated: 2017/03/26 00:30:14 by apoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_conv		*ft_init_conv(void)
 {
 	t_conv	*new;
 
-	if (!(new = malloc(sizeof(t_conv))))
+	if (!(new = ft_memalloc(sizeof(t_conv))))
 		exit(0);
 	new->base = 10;
 	new->space = 0;
@@ -38,7 +38,7 @@ t_arg		*ft_init_arg(void)
 {
 	t_arg	*new;
 
-	if (!(new = malloc(sizeof(t_conv))))
+	if (!(new = ft_memalloc(sizeof(t_conv))))
 		exit(0);
 	new->arg = NULL;
 	new->final_arg = NULL;
@@ -56,7 +56,7 @@ t_uni		*ft_init_uni(void)
 {
 	t_uni	*new;
 
-	if (!(new = malloc(sizeof(t_uni))))
+	if (!(new = ft_memalloc(sizeof(t_uni))))
 		exit(0);
 	new->wc_arg = 0;
 	new->ws_arg = NULL;
@@ -75,7 +75,7 @@ t_data		*ft_init_data(char *format, va_list ap)
 {
 	t_data	*new;
 
-	if (!(new = malloc(sizeof(t_data))))
+	if (!(new = ft_memalloc(sizeof(t_data))))
 		exit(0);
 	new->flag = ft_strdup(" +-#0");
 	new->f_p = ft_strdup("123456789.*");
