a2dd0a7bc24d5eece2bfbb9b09ff79e1822ca7a0
Ozukami <apoisson@student.42.fr>
2017-06-18 05:52:51 +0200

    Discovering Ncurses

---
+++ corewar/srcs/test_ncurses.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ncurses.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apoisson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/18 04:50:28 by apoisson          #+#    #+#             */
+/*   Updated: 2017/06/18 05:51:43 by apoisson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void	init_mem(WINDOW *memory)
+{
+	int		line;
+	int		col;
+	char	*str;
+
+	str = strdup(" 00");
+	line = 1;
+	while (line < 65)
+	{
+		col = 1;
+		while (col < 191)
+		{
+			mvwprintw(memory, line, col, str);
+			col += 3;
+		}
+		line++;
+	}
+}
+
+int		main(void)
+{	
+	WINDOW *memory, *info;
+
+	initscr();
+	noecho();
+	keypad(stdscr, TRUE);
+	curs_set(0);
+	while (1)
+	{
+		memory = subwin(stdscr, 64 + 2, (64 * 2) + 63 + 4, 0, 0);
+		init_mem(memory);
+		info = subwin(stdscr, 64 + 2, 56, 0, (64 * 2) + 63 + 4);
+
+		box(memory, ACS_VLINE, ACS_HLINE);
+		box(info, ACS_VLINE, ACS_HLINE);
+
+		refresh();
+	}
+	endwin();
+	free(memory);
+	free(info);
+	return 0;
+}
--- corewar/test.s
+++ corewar/test.s
@@ -1,13 +1,25 @@
 .comment"comment"
-.name"na
-me"
+.name"name"
 
 label:						#lol
 	sti r1,%:loop,%1		#lol
+	ldi %:loop,%0,r2
+	sti r2,%:label,%-400
+	ldi %:loop,%4,r3
+	#ld :loop,r2
+	sti r3,%:label,%-396
 	and %:loop,r2,r2		##XDQHIU
-	fork %:loop				;mdrliom
-	ld %1,r3#lolefwf
+	fork %:carry			;mdrliom
+	ld %0,r3#lolefwf
+
+jump:
+	zjmp %-500
+
+carry:
+	add r4,r5,r6
+	add r4,r5,r6
 
 loop:
 	live %1
 	zjmp%:loop
+
