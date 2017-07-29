9c22b93b3f446065471aa05d7c72f2af9754238a
qumaujean <quentinmaujean66@gmail.com>
2017-07-29 05:40:03 +0200

    AUTOMATON FREE

--- corewar/srcs/free_auto.c
+++ corewar/srcs/free_auto.c
@@ -6,7 +6,7 @@
 /*   By: qumaujea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/29 03:58:19 by qumaujea          #+#    #+#             */
-/*   Updated: 2017/07/29 05:28:35 by qumaujea         ###   ########.fr       */
+/*   Updated: 2017/07/29 05:39:17 by qumaujea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void			free_automaton_2(t_state *automaton)
 {
+	free((((((automaton->next)[6]->next)[0]->next)[0]->next)[0])->next);
 	free(((((automaton->next)[6]->next)[0]->next)[0]->next)[0]);
 	free((((automaton->next)[6]->next)[0]->next)[0]->next);
 	free((((automaton->next)[6]->next)[0]->next)[0]);
@@ -21,6 +22,7 @@ void			free_automaton_2(t_state *automaton)
 	free(((automaton->next)[6]->next)[0]);
 	free((automaton->next)[6]->next);
 	free((automaton->next)[6]);
+	free(((((automaton->next)[5]->next)[0]->next)[0])->next);
 	free((((automaton->next)[5]->next)[0]->next)[0]);
 	free(((automaton->next)[5]->next)[0]->next);
 	free(((automaton->next)[5]->next)[0]);
@@ -29,6 +31,7 @@ void			free_automaton_2(t_state *automaton)
 	free(((((automaton->next)[4]->next)[1]->next)[0]->next));
 	free((((automaton->next)[4]->next)[1]->next)[0]);
 	free((((automaton->next)[4]->next)[1]->next));
+	free(((((automaton->next)[4]->next)[0]->next)[0])->next);
 	free((((automaton->next)[4]->next)[0]->next)[0]);
 	free(((automaton->next)[4]->next)[1]);
 	free(((automaton->next)[4]->next)[1]);
@@ -39,6 +42,7 @@ void			free_automaton_2(t_state *automaton)
 	free(((automaton->next)[3]->next)[0]);
 	free(((automaton->next)[3]->next));
 	free((automaton->next)[3]);
+	free(((((((automaton->next)[2]->next)[1]->next)[0]->next)[0]->next)[0])->next);
 	free((((((automaton->next)[2]->next)[1]->next)[0]->next)[0]->next)[0]);
 	free((((((automaton->next)[2]->next)[1]->next)[0]->next)[0]->next));
 	free(((((automaton->next)[2]->next)[3]->next)[0]->next)[0]);
@@ -67,6 +71,7 @@ void		free_automaton(t_state *automaton)
 	free(((automaton->next)[2]->next)[0]);
 	free(((automaton->next)[2]->next));
 	free((automaton->next)[2]);
+	free((((((automaton->next)[1]->next)[0]->next)[0]->next)[0])->next);
 	free(((((automaton->next)[1]->next)[0]->next)[0]->next)[0]);
 	free(((((automaton->next)[1]->next)[0]->next)[0]->next));
 	free((((automaton->next)[1]->next)[0]->next));
@@ -74,8 +79,11 @@ void		free_automaton(t_state *automaton)
 	free(((automaton->next)[1]->next)[0]);
 	free(((automaton->next)[1]->next));
 	free((automaton->next)[1]);
+	free(((((automaton->next)[0]->next)[2]->next)[0])->next);
 	free((((automaton->next)[0]->next)[2]->next)[0]);
+	free(((((automaton->next)[0]->next)[1]->next)[0])->next);
 	free((((automaton->next)[0]->next)[1]->next)[0]);
+	free(((((automaton->next)[0]->next)[0]->next)[0])->next);
 	free((((automaton->next)[0]->next)[0]->next)[0]);
 	free((((automaton->next)[0]->next)[2]->next));
 	free((((automaton->next)[0]->next)[1]->next));
