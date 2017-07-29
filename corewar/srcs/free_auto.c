1450ddbd6c406daf8cec66d5d4c1ea82a0c9eec1
qumaujean <quentinmaujean66@gmail.com>
2017-07-29 05:28:59 +0200

    update free x)

--- corewar/srcs/free_auto.c
+++ corewar/srcs/free_auto.c
@@ -6,7 +6,7 @@
 /*   By: qumaujea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/29 03:58:19 by qumaujea          #+#    #+#             */
-/*   Updated: 2017/07/29 05:11:10 by qumaujea         ###   ########.fr       */
+/*   Updated: 2017/07/29 05:28:35 by qumaujea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,16 @@ void			free_automaton_2(t_state *automaton)
 	free(((automaton->next)[5]->next)[0]);
 	free((automaton->next)[5]->next);
 	free((automaton->next)[5]);
+	free(((((automaton->next)[4]->next)[1]->next)[0]->next));
 	free((((automaton->next)[4]->next)[1]->next)[0]);
 	free((((automaton->next)[4]->next)[1]->next));
 	free((((automaton->next)[4]->next)[0]->next)[0]);
-	free(((automaton->next)[4]->next)[0]->next);
+	free(((automaton->next)[4]->next)[1]);
 	free(((automaton->next)[4]->next)[1]);
 	free(((automaton->next)[4]->next)[0]);
 	free(((automaton->next)[4]->next));
 	free((automaton->next)[4]);
+	free((((automaton->next)[3]->next)[0])->next);
 	free(((automaton->next)[3]->next)[0]);
 	free(((automaton->next)[3]->next));
 	free((automaton->next)[3]);
@@ -56,6 +58,7 @@ void			free_automaton_2(t_state *automaton)
 void		free_automaton(t_state *automaton)
 {
 	free_automaton_2(automaton);
+	free(((((automaton->next)[2]->next)[0]->next)[0])->next);
 	free((((automaton->next)[2]->next)[0]->next)[0]);
 	free((((automaton->next)[2]->next)[0]->next));
 	free(((automaton->next)[2]->next)[3]);
